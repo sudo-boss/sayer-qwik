99632978ab0fe33fc683fa0b0b88ff99998d1c22
sb <sb@sb.com>
2023-08-25 20:50:31 -0600

    migration to npm from yarn v2

--- src/appConfig/health/appVersion.ts
+++ src/appConfig/health/appVersion.ts
@@ -1,8 +1,8 @@
-export const commitID = `0c6c49cd46b1455b8bc63d5617d7673349125cc7`;
+export const commitID = `52ba1a255321a1804e9b9be1f4a86ddd4ab0e4ac`;
     
-  export const commitMssg = `migration to npm from yarn`;
+  export const commitMssg = `migration to npm from yarn v2`;
   
-  export const commitDate = `Fri Aug 25 2023 20:50:13 GMT-0600 (Central Standard Time)`;
+  export const commitDate = `Fri Aug 25 2023 20:50:31 GMT-0600 (Central Standard Time)`;
   
   export const commitAuthor = `sb`;
   
